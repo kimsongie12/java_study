f987fc4ddd0e0b6d97104b9ab1688a21c91d8990
kimsongie12 <song3078@naver.com>
2024-03-15 17:06:28 +0900

    java test3-1 문자열 비교

---
+++ src/Test3_1.java
@@ -0,0 +1,16 @@
+//java 문자열 비교 예제
+//
+public class Test3_1 {
+
+    public static void main(String[] args){
+        String s1="홍길동";
+        String s2="배길동";
+
+        if(s1==s2){
+            System.out.println("참조가 같음");
+
+        }else{
+            System.out.println("참조가 다름");
+        }
+    }
+}
